e4241283247ae1f8421560b34ad3404c32fcc345
Michael Bocelli <michael.j.bocelli@gmail.com>
2022-08-05 21:09:12 -0400

    Created method to calculate center of all boids from the perspective of the boid passed as an arg. Also simplified up Boid's class getters and setters.

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -5,28 +5,7 @@ using UnityEngine;
 // For any gameObject which is intended to behave as a Boid, it must utilize this class
 public class Boid : MonoBehaviour
 {
-    Vector3 velocity;
-    bool isPerching;
-
-
-    // Some basic getters and setters for the boid
-    public void SetVelocity(Vector3 newVelocity)
-    {
-        this.velocity = newVelocity;
-    }
-
-    public Vector3 GetVelocity()
-    {
-        return this.velocity;
-    }
-
-    public void SetPerching(bool isPerching)
-    {
-        this.isPerching = isPerching;
-    }
-
-    public bool GetPerching()
-    {
-        return this.isPerching;
-    }
+    // Properties of a boid which are not native to Unity's GameObject
+    public Vector3 velocity { get; set; }
+    public bool isPerching { get; set; }
 }
--- Assets/Scripts/BoidController.cs
+++ Assets/Scripts/BoidController.cs
@@ -17,23 +17,55 @@ public class BoidController : MonoBehaviour
         {
            boids[i] = Instantiate<Boid>(animalPrefab);
         }
+        
         InitBoidPositions();
+
+        for (int i = 0; i < boids.Length; i++) {
+            // Value should be slightly different for each boid
+            print(CalculateCenterOfBoids(boids[i]));
+        }
+    }
+
+    // Move boids each frame based on three rules
+    void Update()
+    {
+        // for each boid
+        //  store results of three rule methods in three vectors
+        //  apply vector results to velocity of current boid
+        //  apply movement each time step based on updated velocity
+        
     }
 
     // Set the boids on the edges of the scene
     void InitBoidPositions()
     {
         int xPos = -9;
-        for (int i = 0; i < boids.Length; i++)
-        {
-            if (i % 2 == 0)
-            {
+        for (int i = 0; i < boids.Length; i++) {
+            if (i % 2 == 0) {
                 xPos = 9;
             } else {
                 xPos = -9;
             }
-
             boids[i].transform.position = new Vector3 (xPos, Random.Range(0.2f, 7f), Random.Range(-9f, 9f));
         }
     }
+
+    // Calculates the center of the flock of boids, ignoring the passed
+    // argument boid's position.
+    Vector3 CalculateCenterOfBoids(Boid boid)
+    {
+        Vector3 totalPositionOfBoids = Vector3.zero;
+
+        for(int i = 0; i < boids.Length; i++)
+        {   
+            // Only consider the position of boids which are not the one passed
+            if (boids[i].transform.position != boid.transform.position) {
+                totalPositionOfBoids += boids[i].transform.position;
+            }
+        }
+
+        Vector3 centerOfBoids = totalPositionOfBoids / (boids.Length - 1);
+        return centerOfBoids;
+    }
+
 }
