9b9ad301fd758bd8dfdb409182dc53c8e2156945
Michael Bocelli <michael.j.bocelli@gmail.com>
2022-08-05 23:50:17 -0400

    Added alignment rule, however, functionality is questionable. Also implemented a speed limit.

--- Assets/Scripts/BoidController.cs
+++ Assets/Scripts/BoidController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class BoidController : MonoBehaviour
 {
-    public int boidAmount = 5;
-    public Boid animalPrefab;
-    public int boidSpacing = 5;
+    public Boid boidPrefab; // Animal to represent a boid
+    public int boidAmount = 5; // Amount of boids (cannot be changed while simulating)
+    public float centeringFactor = 0.0001f; // Speed at which boids rebound out of center
+    public float repulsionFactor = 0.05f; // Speed at which boids turn away from each other (%)
+    public float matchNeighborFactor = 0.05f; // Percentage of neighbors' velocity that will be added to boid
+    public int boidSpacing = 5; // Units between boids allowed before avoiding
+    
     Boid[] boids;
 
     // Initialize array of boids
@@ -16,34 +20,24 @@ public class BoidController : MonoBehaviour
 
         for (int i = 0; i < boids.Length; i++)
         {
-           boids[i] = Instantiate<Boid>(animalPrefab);
+           boids[i] = Instantiate<Boid>(boidPrefab);
         }
 
         InitBoidPositions();
-
-        // Debug
-        for (int i = 0; i < boids.Length; i++) {
-            // Value should be slightly different for each boid
-            print(CalculateCenterOfBoids(boids[i]));
-        }
     }
 
     // Move boids each frame based on three rules
     void Update()
     {
-        // for each boid
-        //  store results of three rule methods in three vectors
-        //  apply vector results to velocity of current boid
-        //  apply movement each time step based on updated velocity
-
-        Vector3 offset1;
-        Vector3 offset2;
+        Vector3 offset1, offset2, offset3;
 
         for (int i = 0; i < boids.Length; i++) {
-            offset1 = Rule1(boids[i]);
-            offset2 = Rule2(boids[i]);
+            offset1 = CohesionOffset(boids[i]);
+            offset2 = AvoidOtherBoids(boids[i]);
+            offset3 = AlignVelocity(boids[i]);
 
-            boids[i].velocity += offset1 + offset2;
+            boids[i].velocity += offset1 + offset2 + offset3;
+            limitBoidVelocity(boids[i]);
             boids[i].transform.position += boids[i].velocity * Time.deltaTime;
             // Rotate boid smoothly (through interpolation) towards its movement dirtection
             boids[i].transform.rotation = Quaternion.Slerp(boids[i].transform.rotation, Quaternion.LookRotation(boids[i].velocity.normalized), Time.deltaTime * 5f);
@@ -66,46 +60,75 @@ public class BoidController : MonoBehaviour
     }
 
     // Boids fly towards the center position of neighboring boids
-    Vector3 Rule1(Boid boid)
+    Vector3 CohesionOffset(Boid boid)
     {
         Vector3 centerOfBoids = CalculateCenterOfBoids(boid);
         // Boids move 0.1% of the way towards the center each frame
-        return (centerOfBoids - boid.transform.position) / 1000;
+        return (centerOfBoids - boid.transform.position) * centeringFactor;
     }
 
-    Vector3 Rule2(Boid boid)
+    Vector3 AvoidOtherBoids(Boid boid)
     {
-        Vector3 moveAwayDistance = Vector3.zero;
+        Vector3 repulsion = Vector3.zero;
 
         for (int i = 0; i < boids.Length; i++) {
             if (boids[i] != boid) {
                 // If the distance between the boid and other is less than the given spacing, then
                 // the boid's position is offset by said distance in the reverse direction
-                if (Mathf.Abs(Vector3.Distance(boids[i].transform.position, boid.transform.position)) < boidSpacing) {
-                    moveAwayDistance -= boids[i].transform.position - boid.transform.position;
+                if (Vector3.Distance(boids[i].transform.position, boid.transform.position) < boidSpacing) {
+                    repulsion -= (boids[i].transform.position - boid.transform.position);
                 }
             }
         }
 
-        return moveAwayDistance;
+        return repulsion * repulsionFactor;
+    }
+
+    Vector3 AlignVelocity(Boid boid)
+    {
+        Vector3 averageVelocity = CalculateAverageVelocity(boid);
+
+        return (averageVelocity - boid.velocity) * matchNeighborFactor;
+    }
+
+    Vector3 CalculateAverageVelocity(Boid boid)
+    {
+        Vector3 averageVelocity = Vector3.zero;
+
+        for (int i = 0; i < boids.Length; i++) {
+            if (boids[i] != boid) {
+                averageVelocity += boids[i].velocity;
+            }
+        }
+        averageVelocity /= (boids.Length -1);
+
+        return averageVelocity;
     }
 
     // Calculates the center of the flock of boids, ignoring the passed
     // argument boid's position.
     Vector3 CalculateCenterOfBoids(Boid boid)
     {
-        Vector3 totalPositionOfBoids = Vector3.zero;
+        Vector3 centerOfBoids = Vector3.zero;
 
-        for(int i = 0; i < boids.Length; i++)
-        {   
+        for (int i = 0; i < boids.Length; i++) {   
             // Only consider the position of boids which are not the one passed
             if (boids[i] != boid) {
-                totalPositionOfBoids += boids[i].transform.position;
+                centerOfBoids += boids[i].transform.position;
             }
         }
+        centerOfBoids /= (boids.Length - 1);
 
-        Vector3 centerOfBoids = totalPositionOfBoids / (boids.Length - 1);
         return centerOfBoids;
+    }
+
+    void limitBoidVelocity(Boid boid) 
+    {
+        int speedLimit = 10;
+        // Magnitude of a boid's velocity is speed
+        if (boid.velocity.magnitude > speedLimit) {
+            boid.velocity = boid.velocity.normalized * speedLimit;
+        }
     }
 
 }
