94658f0de35d664b40c6be93dcd3b358e61ea08f
Michael Bocelli <michael.j.bocelli@gmail.com>
2022-08-09 21:45:14 -0400

    Clamped flock attributes to prohibit complete breakage of simulation.

--- Assets/Scripts/BoidController.cs
+++ Assets/Scripts/BoidController.cs
@@ -21,8 +21,11 @@ public class BoidController : MonoBehaviour
 
     Boid[] boids; // Array of boids
     delegate Vector3 AverageDelegate(Boid boid); // Delegate used to averages of vector attributes of the boids
+
+    // Animation vars
     Animator animator;
     int isPerchedHash;
+    int cycleOffsetHash;
 
     void Start()
     {
@@ -33,8 +36,9 @@ public class BoidController : MonoBehaviour
             boids[i] = Instantiate<Boid>(boidPrefab);
             boids[i].perchingTimer = Random.Range(1f, 5f);
         }
-
+        // Initialize animator vars
         isPerchedHash = Animator.StringToHash("isPerched");
+        cycleOffsetHash = Animator.StringToHash("cycleOffset");
 
         InitBoidPositions();
     }
@@ -42,6 +46,8 @@ public class BoidController : MonoBehaviour
     void HandleBoidAnimations(Boid boid)
     {
         animator = boid.GetComponent<Animator>();
+
+        //animator.SetFloat(cycleOffsetHash, Random.Range(0f, 1f));
 
         if(boid.isPerching){
             animator.SetBool(isPerchedHash, true);
@@ -55,14 +61,22 @@ public class BoidController : MonoBehaviour
     {
         for (int i = 0; i < boids.Length; i++) {
             boids[i].transform.position = new Vector3 (Random.Range(-9f,9f), Random.Range(1f, 7f), Random.Range(-9f, 9f));
-            //boids[i].velocity = new Vector3 (Random.Range(1f, 3f), Random.Range(1f, 3f), Random.Range(1f, 3f));
+            boids[i].velocity = new Vector3 (Random.Range(2f, 5f), Random.Range(2f, 5f), Random.Range(2f, 5f));
         }
+    }
+
+    void HandleFlockAttributes()
+    {
+        centeringFactor = Mathf.Clamp(centeringFactor, 0f, 0.01f);
+        repulsionFactor = Mathf.Clamp(repulsionFactor, 0.05f, 0.1f);
+        matchNeighborFactor = Mathf.Clamp(matchNeighborFactor, 0f, 0.05f);
     }
 
     // Move boids each frame based on three rules
     void Update()
     {
         Vector3 offset1, offset2, offset3, offset4;
+        HandleFlockAttributes();
 
         for (int i = 0; i < boids.Length; i++) {
             HandleBoidAnimations(boids[i]);
@@ -79,7 +93,7 @@ public class BoidController : MonoBehaviour
                 }
             }
             // Gather velocity offsets from rule methods below Update()
-            offset1 = CenterOffset(boids[i]);
+            offset1 = FindFlockCenter(boids[i]);
             offset2 = AvoidOtherBoids(boids[i]);
             offset3 = AlignVelocity(boids[i]);
             offset4 = KeepInBounds(boids[i]);
@@ -91,12 +105,13 @@ public class BoidController : MonoBehaviour
             boids[i].transform.position += boids[i].velocity * Time.deltaTime;
             // Rotate boid smoothly (through interpolation) towards its movement dirtection
             boids[i].transform.rotation = Quaternion.Slerp(boids[i].transform.rotation, Quaternion.LookRotation(boids[i].velocity.normalized), Time.deltaTime * 5f);
+            print(boids[i].velocity.magnitude);
         }
         
     }
 
     // Boids fly towards the center position of other boids
-    Vector3 CenterOffset(Boid boid)
+    Vector3 FindFlockCenter(Boid boid)
     {
         // Average of the positions of all other boids is calculated
         Vector3 centerOfBoids = CalculateAverageOfVectors(boid, boid => boid.transform.position);
@@ -139,7 +154,7 @@ public class BoidController : MonoBehaviour
         foreach (Boid b in boids) {
             // Current boid's vector is excluded from the operation so each boid
             // has its own perspective of the flock.
-            if (b.transform.position != boid.transform.position) {
+            if (b.transform.position != boid.transform.position && boidAmount > 1) {
                 average += vectorValue(b);
             }
         }
@@ -150,7 +165,7 @@ public class BoidController : MonoBehaviour
     // Boid's speed is limited to better simulate real animal speeds
     void LimitBoidVelocity(Boid boid) 
     {
-        int speedLimit = 13;
+        int speedLimit = 10;
         // Magnitude of a boid's velocity is speed
         if (boid.velocity.magnitude > speedLimit) {
             boid.velocity = boid.velocity.normalized * speedLimit;
